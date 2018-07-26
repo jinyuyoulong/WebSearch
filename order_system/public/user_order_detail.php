b846ac3dfe9ad7d2386bd6ee7d290bd7ba76a40a
fans <fan.jinlong@qq.com>
2018-07-26 20:39:13 +0800

    update company

--- order_system/public/user_order_detail.php
+++ order_system/public/user_order_detail.php
@@ -5,7 +5,33 @@
  * Date: 2018/7/13
  * Time: 上午11:18
  */
-include_once 'header.php';?>
+include_once 'header.php';
+
+echo '<div class="container">';
+echo '<table border="1">';
+echo '<tr>';
+echo '<th>时间</th>';
+echo '</tr>';
+
+$dbh = FFPDO::init();
+foreach ($dbh->query('select * from user_order')->fetchAll() as $row){
+    $timeArr[] = $row;
+
+    $datetime = strtotime($row['create_time']);
+    $datetime = date('Y-m-d',$datetime);
+    $orderId = $row['id'];
+    echo '<tr >';
+    $dd = sprintf('<td><a href="user_order_detail.php?oId=%d">%s</a> </td>',$orderId,$datetime);
+    echo $dd;
+//            echo '<td><a href="user_order_detail.php">$row["create_time"]</a> </td>';
+    echo '</tr>';
+
+}
+echo '</table>';
+echo '<a href="user_group_time_orders.php">导出单位时间账单</a>';
+echo '</div>';
+
+?>
 
 		<table border="1">
 			儿童
--- order_system/public/user_select_timer.php
+++ order_system/public/user_select_timer.php
@@ -5,19 +5,35 @@
  * Date: 2018/7/13
  * Time: 上午11:16
  */
-include_once 'header.php';?>
+include_once 'header.php';
+require_once '../FFTools/db_mysql.php';
 
-<table border="1">
-			<thead>
-				<th>时间</th>
+$timeArr = array();
 
-			</thead>
-			<tr >
-				<td><a href="user_order_detail.php">2018.07.14</a> </td>
-			</tr>
-			<tr >
-				<td><a href="user_order_detail.php">2018.07.13</a> </td>
-			</tr>
-</table>
-<a href="user_group_time_orders.php">导出单位时间账单</a>
+echo '<div class="container">';
+echo '<table border="1">';
+        echo '<tr>';
+            echo '<th>时间</th>';
+        echo '</tr>';
+
+$dbh = FFPDO::init();
+foreach ($dbh->query('select * from user_order')->fetchAll() as $row){
+    $timeArr[] = $row;
+    
+    $datetime = strtotime($row['create_time']);
+    $datetime = date('Y-m-d',$datetime);
+    $orderId = $row['id'];
+        echo '<tr >';
+            $dd = sprintf('<td><a href="user_order_detail.php?oId=%d">%s</a> </td>',$orderId,$datetime);
+            echo $dd;
+//            echo '<td><a href="user_order_detail.php">$row["create_time"]</a> </td>';
+        echo '</tr>';
+
+}
+    echo '</table>';
+    echo '<a href="user_group_time_orders.php">导出单位时间账单</a>';
+echo '</div>';
+?>
+
+
 <?php include_once 'footer.php'?>
