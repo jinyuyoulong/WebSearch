fba7578b66f3edb3b4cb4d51af3818e9b6e45db0
fan <fan.jinlong@qq.com>
2018-11-11 17:46:31 +0800

    tp5动态配置

--- tp5/app/index/controller/Index.php
+++ tp5/app/index/controller/Index.php
@@ -3,8 +3,19 @@ namespace app\index\controller;
 
 class Index
 {
+	public function __construct()
+	{
+		config('before','beforeAction');
+	}
     public function index()
     {
+    	config('indexAction','index');
        dump(config());
+       
+    }
+
+    public function demo()	
+    {
+    	dump(config());
     }
 }
