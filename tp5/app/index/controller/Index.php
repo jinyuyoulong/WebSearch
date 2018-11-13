ce596bda890e2d55f0a96e243fee5ec3b7f2dd23
fans <fan.jinlong@qq.com>
2018-11-13 14:31:34 +0800

    比较标签

--- tp5/app/index/controller/Index.php
+++ tp5/app/index/controller/Index.php
@@ -7,24 +7,9 @@ class Index extends Controller
 {
     public function index()
     {
-        $list = [
-            'user1' => [
-                'name' => 'fan',
-                'email'=> 'fan@qq.com'
-                ],
-            'user2' =>[
-              'name'    =>  'jin',
-              'email'   =>  'jin@qq.com'
-            ],
-            'user3' => [
-                'name'  =>  'long',
-                'email' =>  'long@qq.com'
-            ]
-        ];
-//        $list = [];
-//        $empty = "<h1>这里分配为空</h1>";
-//        $this->assign('empty',$empty);
-        $this->assign('list',$list);
+
+        $this->assign('a',10);
+        $this->assign('b',20);
         return $this->fetch();
     }
 
