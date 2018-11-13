775dd7c574a369c18e894d7ea3fdddc4f8bbff20
fans <fan.jinlong@qq.com>
2018-11-13 18:19:22 +0800

    链式操作

--- tp5/app/index/controller/Index.php
+++ tp5/app/index/controller/Index.php
@@ -8,42 +8,8 @@ class Index extends Controller
     public function index()
     {
         $db = Db::name('user');
-
-        # where(['id'=> 1])
-        # where("id=1")
-        # where("id",1)
-        # where('id','<>',1) id不等于1，参数：字段，表达式，值
-
-        # 表达式   不区分大小写，
-        # EQ    =
-        # NEQ   <>
-        # LT    <
-        # ELT   <=
-        # RT    >
-        # ELT   >=
-        # BETWEEN   BETWEEN * AND *
-        # NOTBETWEEN    NOTBETWEEN * AND *
-        # IN    IN (*,*)
-        # NOTIN NOT IN (*,*)
-
-        # 写法    建议使用三个参数的方式，两个的话建议使用数组的方式
         
-//        $sql = $db->where('id','between',"1,5")->buildSql();
-
-//        $sql = $db->where([
-//            'id'=> ['in',[1,3,5]],
-//            'username' => 'fans'
-//        ])->buildSql();
-
-//        $sql = $db->where('id','EXP',"NOT in (1,2,3)")
-//            ->buildSql();
-
-        $sql = $db->where('id','in', '1,2,3')
-            ->where('username',"eq","fan")
-            ->whereOr('email','eq','fan@qq.com')
-            ->buildSql();
-
-        dump($sql);
+        dump($result);
 
     }
 }
