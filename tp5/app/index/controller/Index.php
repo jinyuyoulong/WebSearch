0ec73cbcc1d8ecc386eff0556cb21e829a46a9c2
fans <fan.jinlong@qq.com>
2018-11-13 17:39:24 +0800

    DB delete

--- tp5/app/index/controller/Index.php
+++ tp5/app/index/controller/Index.php
@@ -8,26 +8,9 @@ class Index extends Controller
     public function index()
     {
         $db = Db::name('user');
-        # update 返回影响的行数量
-//        $result = $db->where([
-//            'id'=>2
-//        ])->update([
-//           'username' => 'fan',
-//            'email' => 'fan@qq.com'
-//        ]);
-
-        # setField 每次只更新一个字段，返回影响的行数量
-//        $result = $db->where(['id'=>3])->setField('username','jin');
-
-        # setInc    字段自增，默认自增1，返回影响的行数量
-//        $result = $db->where([
-//            'id'=>1,
-//        ])->setInc('num',5);
-
-        # setDec 自减
-        $result = $db->where([
-            'id'=>1
-        ])->setDec('num',5);
+        # delete 如果操作主键，可以直接传入主键的值，进行delete
+        # where("1=1") where 永远返回 true 会将数据全部删除，不建议使用。
+        $result = $db->where("1=1")->delete();
 
         dump($result);
 
