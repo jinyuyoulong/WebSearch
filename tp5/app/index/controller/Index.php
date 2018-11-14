11d96da3ece50faf5c7598e869b40940dad92236
fans <fan.jinlong@qq.com>
2018-11-14 15:00:08 +0800

    数据库链式操作

--- tp5/app/index/controller/Index.php
+++ tp5/app/index/controller/Index.php
@@ -7,8 +7,16 @@ class Index extends Controller
 {
     public function index()
     {
-        $db = Db::name('user');
-        
+
+        $result = Db::table('imooc_user')
+//            ->where('id',">",10)
+            -> field("username, id")
+//            ->order("id DESC") //使用group 时order不生效
+//            ->limit('3',5)
+//                -> page(2,5)//limit((2-1)*5,5)
+            -> group("mgroup") //按group不同分组，相同则只返回一条数据
+            -> select();
+
         dump($result);
 
     }
