dac8049cb41f4b955240fe104b20bf64ca068942
fans <fan.jinlong@qq.com>
2018-11-14 17:33:39 +0800

    use model insert data

--- tp5/app/index/controller/Index.php
+++ tp5/app/index/controller/Index.php
@@ -1,48 +1,50 @@
 <?php
 namespace app\index\controller;
 use think\Controller;
-use think\Db;
 use app\index\model\User;
-use think\Loader;
 
 class Index extends Controller
 {
     public function index()
     {
 
-        $result = User::get(1);
-//        $result = User::get(function ($query){
-//            $query->where("username","eq","imooc_1")
-//                ->field('username, email'); // 注意 field只传入了一个参数
-//        });
+//        $result = User::create([
+//           'username'   =>  "imooc",
+//           'email'      =>  "imooc@qq.com",
+//           'password'   =>  md5('imooc'),
+//           'num'        =>  100,
+//            'demo'      =>  "demo"
+//        ],['username','email']);
+        # create 第二个参数 true，排除非数据库字段
+        # create 第二个参数 数组，指定需要添加进数据库的字段
 
-//        $result = User::where('id',2)
-//            ->find();
+//        $userModel = new User;
+//        $userModel->username = 'fan';
+//        $userModel->email = 'fan@qq.com';
+//        $userModel->save();// 私有方法
+        $userModel = new User;
+//        $userModel->save([
+//            'username' => 'fan',
+//            'password' => md5('fan')
+//        ]);
 
-//        dump($result->username);
+        # allowField 方法 同上边 create方法第二个参数的作用
+//        $userModel->allowField(['username'])->save([
+//            'username' => 'fan',
+//            'password' => md5('fan'),
+//            'demo'=>'demo'
+//        ]);
 
-//        $result = User::all("1,2,3");
-//        $result = User::all([1,2,3]);
-//        $result = User::all(function ($query){
-//            $query->where("id", "<", "5")
-//            ->field("username,id");
-//        });
+        $result = $userModel->saveAll([
+            ['email'=>   "jin@qq.com"],
+            ['email' =>  "long@qq.com"]
+        ]);
 
-//        $result = User::where("id", ">", 15)
-//            -> field("id, username")
-//            -> limit(3)
-//            -> order("id DESC")
-//            ->select();
-//        foreach ($result as $value)
-//        {
-//            $result = $value->toArray();
-//            dump($result);
-//        }
-
-//        $result = User::where("id",10)->value('id');
-        $result = User::column("email","id"); //返回数组：id:email 格式
-
-        dump($result);
-
+        foreach ($result as $value)
+        {
+            dump($value->toArray());
+        }
+//        dump($userModel->id);
+//        dump($result);
     }
 }
