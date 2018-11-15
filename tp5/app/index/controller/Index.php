38ac995c1ae049de4e0c03ea1aa302def45fead6
fans <fan.jinlong@qq.com>
2018-11-15 11:46:24 +0800

    model attrubute 模型获取器 mode translate source data to target data

--- tp5/app/index/controller/Index.php
+++ tp5/app/index/controller/Index.php
@@ -7,26 +7,9 @@ class Index extends Controller
 {
     public function index()
     {
-         // $result = User::count();
-        // $result = User::where('id',">",5)
-        // -> count();
-
-        # 最大值
-        // $result = User::max('num');
-
-        // $result = User::where('id','<',25)
-        // -> max('num');
-
-        # 求和
-        // $result = User::sum('num');
-        // $result = User::where("id", "<", 24)
-        // -> sum('num');
-
-        # 平均值
-        // $result = User::avg('num');
-
-        # 最小值
-        $result = User::min('num');
-         dump($result);
+        $result = User::get(3);
+        echo $result->sex;
+        dump($result->toArray());
+        dump($result->getData());
     }
 }
--- tp5/app/index/model/User.php
+++ tp5/app/index/model/User.php
@@ -14,4 +14,18 @@ class User extends Model
     # 命名 imooc_user  ->     User.php    User
     # imooc_user_info ->    UserInfo.php  UserInfo
 
+    public function getSexAttr($value)
+    {
+        switch ($value){
+            case "1":
+                return "男";
+                break;
+            case "0":
+                return "女";
+                break;
+            default:
+                return "未知";
+                break;
+        }
+    }
 }
