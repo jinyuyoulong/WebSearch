366c08226f2d075865f5bae488488426d477ff95
fan <fan.jinlong@qq.com>
2019-12-04 23:06:57 +0800

    jquery start

---
+++ biaoyansu/jQuery/1.1/main.js
@@ -0,0 +1,10 @@
+document
+    .getElementById('a')
+    .style
+    .background = 'blue';
+
+$('#b')
+    .css('background','green');
+$('#a p').css('border','2px solid brown');
+$('.b').css('background','pink')
+$('input[type="number"]').css('background','yellow');
