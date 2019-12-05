b34f9a83a18d34695ad7974ddd75f8ae90964388
fans <fan.jinlong@qq.com>
2019-12-05 11:47:10 +0800

    jquery DOM操作

---
+++ biaoyansu/jQuery/1.4-DOM/main.js
@@ -0,0 +1,13 @@
+// text
+// html
+// prepend
+// append
+// remove
+
+console.log($('#a').text())
+console.log($('#a').html())
+
+$('#b').prepend('prepend<br>')
+$('#b').append('<br>append')
+
+$('#b span').remove()
