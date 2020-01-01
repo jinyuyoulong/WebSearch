3267108983700e3215185c2e3201dc21471563fc
fans <fan.jinlong@qq.com>
2020-01-01 08:42:53 +0800

    dom 完成

--- biaoyansu/jquery-DOM/htmlFormElement/main.js
+++ biaoyansu/jquery-DOM/htmlFormElement/main.js
@@ -12,19 +12,15 @@
 
 
 
-var el = document.querySelector('input')
-var el1 = document.querySelector('#checkbox')
-var el2 = document.querySelector('#text')
-var info = document.querySelector('#info')
-info.innerHTML =
-    '<div><samp><strong>name</strong>:' + el.name + '</samp></div>' +
-    '<div><samp><strong>type</strong>:' + el.type + '</samp></div>' +
-    '<div><samp><strong>disabled</strong>:' + el.disabled + '</samp></div>' +
-    '<div><samp><strong>required</strong>:' + el.required + '</samp></div>' +
-    '<div><samp><strong>autofocus</strong>:' + el.autofocus + '</samp></div>' +
-    '<div><samp><strong>value</strong>:' + el.value + '</samp></div>'+
-    '<div><samp><strong>checked</strong>:' + el1.checked + '</samp></div>'+
-    '<div><samp><strong>defaultChecked</strong>:' + el.defaultChecked + '</samp></div>'+
-    '<div><samp><strong>autocomplete</strong>:' + el2.autocomplete + '</samp></div>'+
-    '<div><samp><strong>placeholder</strong>:' + el2.placeholder + '</samp></div>'+
-    '<div><samp><strong>readOnly</strong>:' + el2.readOnly + '</samp></div>'
+var el = document.querySelector('form')
+console.log("elements:"+el.elements)
+console.log("length:" +el.length)
+console.log("method:" +el.method)
+console.log("action:" +el.action)
+console.log("autocomplete 是否允许浏览器自动补全:" + el.autocomplete)
+console.log("noValidate 是否不做表单验证:" + el.noValidate)
+
+function formReset() {
+    el.reset()
+    console.log('execute reset form')
+}
