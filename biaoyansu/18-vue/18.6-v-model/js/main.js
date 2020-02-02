0e0c491c594dcce0a3076a0761255fc0f2d7d3d8
fans <fan.jinlong@qq.com>
2020-02-02 15:03:05 +0800

    Vue v-model实践

---
+++ biaoyansu/18-vue/18.6-v-model/js/main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: "#app",
+    data:{
+        // sex: ['female'],
+        // article: ' Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad voluptatum dignissimos neque error nostrum alias, odit harum quidem! Distinctio sit neque deserunt id praesentium odio. Soluta, natus. Rem, qui ut.',
+        from: 1,
+        dest: []
+    }
+})
